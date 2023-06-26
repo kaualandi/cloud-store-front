f533a6c7b886a5d6538d0cba4f4f93b62dae1f78
Kauã Landi <eu@kaualf.com>
2023-06-26 20:55:40 -0300

    feat: added articles section in home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,9 +7,15 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HeroComponent } from '../../components/home/hero/hero.component';
 import { SwiperModule } from 'swiper/angular';
 import { TeamsSellersComponent } from '../../components/home/teams-sellers/teams-sellers.component';
+import { ArticlesComponent } from 'src/app/components/home/articles/articles.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, HomeRoutingModule, SwiperModule],
-  declarations: [HomeComponent, HeroComponent, TeamsSellersComponent],
+  declarations: [
+    HomeComponent,
+    HeroComponent,
+    TeamsSellersComponent,
+    ArticlesComponent,
+  ],
 })
 export class HomeModule {}
