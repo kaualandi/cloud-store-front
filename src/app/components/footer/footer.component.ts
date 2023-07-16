3dce7b25f27cc512a9bf7800a402dfb2adb4a466
Kauã Landi <eu@kaualf.com>
2023-07-16 10:30:17 -0300

    feat: completed footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  mail = 'mailto:contato@cloudstore.com';
+  wpp = 'https://wa.me/5521996953637';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  instagram = 'https://www.instagram.com/cloudstorebr/';
+  facebook = 'https://www.facebook.com/cloudstorebr';
+  tiktok = 'https://www.tiktok.com/@cloudstorebr';
 }
