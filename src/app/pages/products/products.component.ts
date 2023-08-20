8c817b264a66833f552fbccce7ac4031bf5ae4d6
Kauã Landi <eu@kaualf.com>
2023-08-19 22:43:10 -0300

    feat: starts products page

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { IProduct } from 'src/app/models/product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  products: IProduct[] = [];
+  orderBy = new FormControl('release');
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { SharedModule } from 'src/app/components/shared/shared.module';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule, SharedModule, ProductsRoutingModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ProductsRoutingModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ProductsModule {}
